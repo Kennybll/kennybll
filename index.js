c6d54f057161e938331161ca88e3ae7dd6f160b7
Kenneth Bennett <Kennybll@outlook.com>
2017-03-27 14:59:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 80;
 
-
-//app.configure(function () {
-//    app.use(express.static(path.join(__dirname, 'public')));
-//});
 app.get('/', function (req, res) {
-    res.send("Come back soon");
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 var server = http.createServer(app).listen(port, function () {
